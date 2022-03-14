33e76b0bbd9b7c000ed37872034e7cc4f07ad917
YosiLebman <yosil.770@gmail.com>
2022-03-14 11:41:58 +0200

    basic express template & created route files

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("here will be the main page")
+// imports
+const express = require('express')
+const cookieParser = require('cookie-parser')
+
+// inits
+const app = express()
+
+// middlewares
+app.use(express.json())
+app.use(cookieParser())
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/profile', require('./routes/profile'))
+app.use('/api/tweets', require('./routes/tweets'))
+app.use('/api/comments', require('./routes/comments'))
+app.use('/api/likes', require('./routes/likes'))
+
+// listen
+app.listen(1000, ()=>console.log("server up and runing on port 1000"))
