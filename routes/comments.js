c4680a147f47708e9d86cd6dd2b5fc3044e02d2b
Yosi Leibman <yosil.770@gmail.com>
2022-03-16 12:59:33 +0200

    comments routes done and tested

--- routes/comments.js
+++ routes/comments.js
@@ -1,12 +1,43 @@
+const { v4 } = require('uuid')
+const { db } = require('../db/db')
+const { onlyLogged } = require('../helpers/onlyLogged')
+
 const router = require('express').Router()
 
 
-router.get('/:tweet_id', (req, res) => {
+router.get('/:author/:tweet_id', (req, res) => {
+    const { author, tweet_id } = req.params
+
+    if (!author || !tweet_id) {
+        return res.status(400).send({ err: 'missing some info' })
+    }
+
+    const user = db.find(ur => ur.username == author)
+
+    const tweet = user.tweets.find(t => t.id == tweet_id)
+
+    res.send(tweet.comments)
 
 })
 
-router.post('/', (req, res) => {
+router.post('/', onlyLogged, (req, res) => {
 
+    const { author, text, tweet_id } = req.body
+
+    if (!author || !tweet_id || !text) {
+        return res.status(400).send({ err: 'missing some info' })
+    }
+
+    const user = db.find(ur => ur.username == author)
+
+    const tweet = user.tweets.find(t => t.id == tweet_id)
+  
+    tweet.comments.push({
+        published:new Date(),
+        text,
+        username:req.user.username
+    })
+    res.send({msg:'your comment submitted, we will publish it soon'})
 })
 
 module.exports = router
