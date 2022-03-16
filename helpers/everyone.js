1fc33d6430da45ecc8c2d5823c6e1c0a2ee4b799
Yosi Leibman <yosil.770@gmail.com>
2022-03-16 11:38:47 +0200

    profile routes done and tested

---
+++ helpers/everyone.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken")
+
+module.exports.everyone = function (req, res, next) {
+    try {
+        const dec = jwt.decode(req.cookies.sid)
+        if (dec) {
+            req.user = dec
+        } else {
+            req.user = false
+        }
+        next()
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
+}
+
+
+// jwt.sign ==> generates new token
+// jwt.verify ==> checks the Authentication
+// jwt.decode ==> skips the checking, just retuns the payload
--- routes/profile.js
+++ routes/profile.js
@@ -3,21 +3,40 @@ const { onlyLogged } = require('../helpers/onlyLogged')
 
 const router = require('express').Router()
 
-router.get('/', onlyLogged, (req, res) => {
-    const { username } = req.user
+router.get('/', (req, res) => {
+    const { username } = req.query
     const user = db.find(ur => ur.username == username)
     res.status(user ? 200 : 404).send(user || { err: 'profile not found' })
 })
 
-router.put('/avatar', (req, res) => {
-
+router.put('/avatar', onlyLogged, (req, res) => {
+    const user = db.find(ur => ur.username == req.user.username)
+    user.avatar = req.body.avatar
+    res.send({msg:'avatar updataed, great pic btw!'})
 })
 
-router.put('/nickname', (req, res) => {
-
+router.put('/nickname', onlyLogged, (req, res) => {
+    const user = db.find(ur => ur.username == req.user.username)
+    user.nickname = req.body.nickname
+    res.send({msg:'nick updataed, cool name bruh!'})
 })
 
-router.post('/follow', (req, res) => {
+router.post('/follow', onlyLogged, (req, res) => {
+    
+    // check for taken usernames
+    if (!db.some(user => user.username == req.body.username)) {
+        return res.status(400).send({ err: "you can't have imaginary friends here =(" })
+    }
+
+    const user = db.find(ur => ur.username == req.user.username)
+    
+    if (user.following.includes(req.body.username)) {
+        user.following = user.following.filter(un=>un != req.body.username)
+        res.send({msg:"unfollowing " + req.body.username})
+    }else{
+        user.following.push(req.body.username)
+        res.send({msg:"now you're following " + req.body.username})
+    }
 
 })
 
