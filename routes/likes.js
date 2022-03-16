6df9e29d93081c9d6d2b6aa668d741d231aedbc5
Yosi Leibman <yosil.770@gmail.com>
2022-03-16 13:12:32 +0200

    likes routes done and tested

--- routes/likes.js
+++ routes/likes.js
@@ -1,6 +1,23 @@
+const { db } = require('../db/db')
+const { onlyLogged } = require('../helpers/onlyLogged')
+
 const router = require('express').Router()
 
-router.post('/', (req, res) => {
+router.post('/', onlyLogged, (req, res) => {
+
+    const { tweet_id, author } = req.body
+
+    const user = db.find(ur => ur.username == author)
+
+    const tweet = user.tweets.find(t => t.id == tweet_id)
+
+    if (tweet.likes?.includes(req.user.username)) {
+        tweet.likes = tweet.likes.filter(un => un != req.user.username)
+        res.send({ msg: 'think about it again :(' })
+    } else {
+        tweet.likes?.push(req.user.username)
+        res.send({ msg: 'thanks! ervery like is counted!' })
+    }
 
 })
 
